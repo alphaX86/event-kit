01f33ff1980cf13ef1c2792c5367bd3f9eee2f4f
Aadhitya A <aadhitya864@gmail.com>
2023-03-27 15:25:50 +0530

    Make changes
    Signed-off-by: Aadhitya A <aadhitya864@gmail.com>

--- lib/constants.ts
+++ lib/constants.ts
@@ -17,16 +17,16 @@
 export const SITE_URL = 'https://demo.vercel.events';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
 export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const BRAND_NAME = 'Samhita 2023';
+export const SITE_NAME_MULTILINE = ['Samhita', ''];
+export const SITE_NAME = 'Samhita - ITA';
 export const META_DESCRIPTION =
   'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
 export const SITE_DESCRIPTION =
   'An interactive online experience by the community, free for everyone.';
-export const DATE = '01 January 2022';
-export const SHORT_DATE = 'Jan 1 - 9:00am PST';
-export const FULL_DATE = 'Jan 1st 9am Pacific Time (GMT-7)';
+export const DATE = '15-16 April 2023';
+export const SHORT_DATE = 'April 15 - 9:00am IST';
+export const FULL_DATE = 'Apr 15th 9am Indian Standard Time (GMT+5:30)';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
@@ -41,16 +41,12 @@ export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
-    name: 'Live Stage',
-    route: '/stage/a'
+    name: 'Workshops',
+    route: '/stage/event-a'
   },
   {
-    name: 'Vercel Stage',
-    route: '/stage/c'
-  },
-  {
-    name: '100ms Stage',
-    route: '/stage/m'
+    name: 'Events',
+    route: '/stage/event-b'
   },
   {
     name: 'Schedule',
@@ -65,8 +61,12 @@ export const NAVIGATION = [
     route: '/expo'
   },
   {
-    name: 'Jobs',
-    route: '/jobs'
+    name: 'Sponsors',
+    route: '/sponsors'
+  },
+  {
+    name: 'Contact',
+    route: '/contact'
   }
 ];
 
